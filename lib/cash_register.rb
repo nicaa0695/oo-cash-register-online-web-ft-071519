cf8aa71f6833d94c22b553272016c22ab8238819
Veronica Lopez <nicaa0695@gmail.com>
2019-07-27 17:41:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,7 +20,7 @@ class CashRegister
   end  
   
   def apply_discount
-    if discount == 0 
+    if @discount == 0 
       "There is no discount to apply."
     else self.total -= (0.01 * @discount * @total).to_i 
       "After the discount, the total comes to #{self.total}."
